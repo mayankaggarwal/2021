a46aedfbb2a1c07d3f2314dfb9fa4b23cdd0c511
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2021-05-30 18:34:52 +0530

    Adding Sample Api Test

---
+++ SampleApi/mydev.SampleProject/mydev.SampleProject.WebApiClient.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace mydev.SampleProject.WebApiClient.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ SampleApi/mydev.SampleProject/mydev.SampleProject.WebApiClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mydev.SampleProject.WebApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
